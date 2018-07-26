96969b581418139e4a63425e9f7515bed260b665
CK <CK@sina.com>
2018-07-26 11:22:27 +0800

    初始化项目和首页静态结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router';
+import './assets/statics/site/css/style.css';
+
 Vue.config.productionTip = false
 
+import index from './components/index.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes:[
+      { path: '/', component: index },
+      { path: '/index', component: index }
+    ]
+})
+
 new Vue({
+  el:'#app',
+  router,
   render: h => h(App)
-}).$mount('#app')
+})
